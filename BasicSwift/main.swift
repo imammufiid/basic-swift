c5bade0d66f006ada2fd7389c357782be82f320a
imufiid <imufiid98@gmail.com>
2021-12-04 06:29:16 +0700

    Basic array in swift

--- BasicSwift/main.swift
+++ BasicSwift/main.swift
@@ -7,5 +7,16 @@
 
 import Foundation
 
-print("Hello, World!")
+var strArray = ["Imam", "Mufiid", "asfdasfadf"]
+var intArray = [1, 2, 3, 4, 5]
 
+// var anyArray = ["Imam", "Mufiid", 2] // ERROR
+// solution from above variable is
+var anyArray2 = ["Imam", "Mufiid", 2, true, 1.6] as [Any]
+
+// Getting item in array with index
+print(strArray[0])
+print(intArray[2])
+
+// Getting total item in array
+print(strArray.count)
