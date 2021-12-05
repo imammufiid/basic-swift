024e374e3cf532d122b2f26b1ef589f31c5455c8
imufiid <imufiid98@gmail.com>
2021-12-05 09:23:14 +0700

    For loop

--- BasicSwift/main.swift
+++ BasicSwift/main.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-var numbs = [1,2,3,4,5,6,7,8,9,0]
+var numbs = ["Apple", "Orange", "Grape", "Banana"]
 
 // While Loop
 
@@ -15,4 +15,13 @@ var i = 0
 while i < 10 {
     print(i)
     i += 1
+}
+
+// For Loop
+for numb in numbs {
+    print(numb)
+}
+
+for numb in 1...3 {
+    print(numb % 3)
 }
