1c98e57b3b6c338c7d8d1da6a4174f90a445f0dc
imufiid <imufiid98@gmail.com>
2021-12-05 10:41:52 +0700

    Basic function

--- BasicSwift/main.swift
+++ BasicSwift/main.swift
@@ -7,5 +7,28 @@
 
 import Foundation
 
-print("Hello, World!")
+myFunction()
+// Basic function
+func myFunction() {
+    print("Call myFunction")
+}
 
+
+// Params in function
+func calculate(x: Int, y: Int) {
+    print("Sum : \(x+y)")
+}
+calculate(x: 1, y: 9)
+
+
+// Return Type function
+func timeCalculate(x: Int, y: Int) -> Int {
+    x * y
+}
+print("Time: \(timeCalculate(x: 2, y: 2))")
+
+func isValid(name: String) -> Bool {
+    !name.isEmpty
+}
+print("NAME VALID: \(isValid(name: ""))")
+print("NAME VALID: \(isValid(name: "Imam"))")
