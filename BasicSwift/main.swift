331315f75b79bc0640fae6fa7fe732f618e702b8
imufiid <imufiid98@gmail.com>
2021-12-05 09:29:56 +0700

    Basic if else condition and switch case

--- BasicSwift/main.swift
+++ BasicSwift/main.swift
@@ -7,5 +7,24 @@
 
 import Foundation
 
-print("Hello, World!")
+// If condition
+print("------ IF ELSE ------")
+var age = 6
+if age < 17 {
+    print("Dibawah umur")
+} else if age > 17 && age < 40 {
+    print("Produktif")
+} else {
+    print("Manula")
+}
 
+// Switch case
+print("------ SWITCH CASE ------")
+switch age {
+    case 0...17:
+        print("Dibawah umur")
+    case 17...40:
+        print("Produktif")
+    default:
+        print("Manula")
+}
