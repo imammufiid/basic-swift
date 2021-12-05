5a9cec548797d140422d27bd160cc67d6119fc30
imufiid <imufiid98@gmail.com>
2021-12-05 10:56:57 +0700

    Basic optional, create default value if return nil. In Kotlin this is a null safe

--- BasicSwift/main.swift
+++ BasicSwift/main.swift
@@ -7,5 +7,25 @@
 
 import Foundation
 
-print("Hello, World!")
+// Optional -> Null safe in Kotlin
+// default value is nil
+
+var myName: String?
+print(myName?.uppercased() ?? "default value if return nil") // return nil
+
+myName = "Imam"
+print(myName?.uppercased() ?? "-")
+
+var myAge = "0"
+
+// Don't
+// var myInt = (Int(myAge) ?? 0) / 5
+
+// Solution line 21
+if let myNumb = Int(myAge) {
+    print(myNumb * 5)
+} else {
+    print("Wrong input")
+}
+
 
